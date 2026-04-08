0873a7e5212e1cff2da981d11507a16b03af56f9
VishnuKanth <vishnukanth512@gmail.com>
2026-04-08 16:12:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+// console.log("Captain Cool")
+
+
+// !-- variable
+
+//var,let,const - keywords for variable 
+
+// var a =10;
+// var username = 'vishnu'
+// let b=7;
+
+
+// var a; // only declaration is possible
+
+// a=10; // intialize in different line is possible 
+
+// console.log(a);
+
+// {
+//     let a = 10;
+
+// }
+
+// console.log(a); 
+
+// function add(){
+//     var a=10;
+// }
+
+// add();
+// console.log(a);
+
+// let username = 'vishnu';
+// const college = "LPU";
+// let deatils = `I am ${username} from ${college}`;
+// console.log(deatils);
+
+// let deatils = "i am from 'LPU' punjab";
+// console.log(deatils);
+
+// boolean
+
+// let isMarried = '20';
+// console.log(isMarried);
+// console.log(typeof isMarried);
+
+// undefined
+
+// let a ;
+// console.log(a);
+
+// let arr = ["vishnu",21,true]
+// console.log(arr); 
+
+
+// let a =10;
+// let b=20;
+// let res = a+b;
+// console.log(res);
+
+
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a==b);
+// console.log(a===b);
+
+// let username = 'vishnu';
+// console.log(username.length);
+// username=username.toUpperCase;
+// console.log(username);
+
+// let locatio = "punjab";
+
+// let replace = locatio.replace('p','g');
+// console.log(replace);
+
+let locatio = "india";
+let replace = locatio.replace('i','s');
+console.log(replace);
+
+let replaceAll = locatio.replaceAll('i','s');
+console.log(replaceAll);
+
+
+
+
+
+
+
+
