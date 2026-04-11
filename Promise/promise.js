dbbf283c4c3226a47dbb6622932de10ba5750691
VishnuKanth <vishnukanth512@gmail.com>
2026-04-11 12:15:03 +0530

    Promises

---
+++ Promise/promise.js
@@ -0,0 +1,25 @@
+// promise is an object which represent eventual compilition or failure of an asynchronous task
+
+// 3 states in promise
+// !-- pending
+// !-- fulfilled
+// !-- rejected
+
+// let myPromise = new Promise((res,rej)=>{
+
+//     let pizzaReady = true;
+//     if(pizzaReady){
+//         res('Pizza is ready')
+//     }else{
+//         rej('pizza is not available..')
+//     }
+
+// })
+// console.log(myPromise);
+
+// myPromise.then(t=>console.log(t))
+//          .catch(c=>console.log(c))
+//          .finally(()=>console.log('I am a finally'));
+
+//=============================================================================================================
+
