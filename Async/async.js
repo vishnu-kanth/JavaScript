56dfb7a3879c997680c638284cd00a5bbf8a84c1
VishnuKanth <vishnukanth512@gmail.com>
2026-04-13 16:27:50 +0530

    Async

---
+++ Async/async.js
@@ -0,0 +1,48 @@
+
+// !-- setTimeout()
+
+
+// console.log('start');
+    
+//     setTimeout(()=>{
+//         console.log('hello');
+        
+//     },2000);
+// console.log('end');
+
+//!-- setInterval
+
+// console.log('start');
+
+// setInterval(()=>{
+//     console.log('hello');
+    
+// },2000);
+// console.log("end");
+
+
+//!---clearInterval
+
+// let greet = setInterval(()=>{
+//     console.log('hello...');
+    
+// },1000)
+
+// setTimeout(()=>{
+//     // clearInterval(greet)
+//     clearTimeout(greet)
+// },3000)
+
+// !-- clearTimeOut
+
+// let greet = setInterval(()=>{
+//     console.log('hello...');
+    
+// },1000)
+
+// setTimeout(()=>{
+
+//     clearTimeout(greet)
+// },3000)
+
+
