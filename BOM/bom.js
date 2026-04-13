725cdf7a512564e3f43e3c77090ed7e6551c7e4d
VishnuKanth <vishnukanth512@gmail.com>
2026-04-13 16:28:30 +0530

    Browser Object Model

---
+++ BOM/bom.js
@@ -0,0 +1,33 @@
+//! -- BOM (Browser Object Model)
+
+console.log(window);
+
+// var a = 10;
+
+// let a1 = 22;
+
+// // var aab = 30;
+
+// console.log(a);
+
+// ! -- prompt 
+
+
+
+// let data = prompt('Enter username');
+// console.log(data);
+
+// let res = Number(data);
+// console.log(typeof res);
+
+// !--- alert
+
+// window.alert('data submitted');
+
+// !--- confirm()
+
+// window.confirm('Are you sure ')
+
+
+
+
